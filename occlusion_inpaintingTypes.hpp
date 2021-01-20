d846c0e3c1414b38de61a0a8cbe71e2eb579fdba
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-01-20 09:58:08 +0000

    Init repository

---
+++ occlusion_inpaintingTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef occlusion_inpainting_TYPES_HPP
+#define occlusion_inpainting_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace occlusion_inpainting {
+}
+
+#endif
+
